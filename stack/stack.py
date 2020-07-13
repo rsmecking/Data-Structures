8152c0bd96dda2ea56f3d121c2d118457a780cce
Ryan Mecking <rsmecking@gmail.com>
2020-07-13 19:28:09 -0400

    pushing to new branch

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,4 @@
+from .singly_linked_list.singly_linked_list import Node, Linkedlist
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
